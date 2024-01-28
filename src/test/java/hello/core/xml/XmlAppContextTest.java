631c834a693b2d7224608e34f065be8b3ff8fc71
SeungYeop-Han <ammezkhan@gmail.com>
2024-01-28 18:43:03 +0900

    test: xml 구성 파일을 이용하여 스프링 컨테이너 만들기

---
+++ src/test/java/hello/core/xml/XmlAppContextTest.java
@@ -0,0 +1,16 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContextTest {
+
+    @Test
+    void xml을_이용한_빈_구성_정보_제시() {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
